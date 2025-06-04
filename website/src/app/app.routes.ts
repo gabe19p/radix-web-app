f7fca4d2b332bcd9a4664962486313640848e672
gabe19p <g19purselley@gmail.com>
2025-06-04 10:35:12 -0600

    who-we-are page added

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { WhoWeAre } from './pages/who-we-are/who-we-are';
 
-export const routes: Routes = [{ path: '', component: Home }];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'who-we-are', component: WhoWeAre },
+];
---
+++ website/src/app/pages/who-we-are/who-we-are.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-who-we-are',
+  imports: [],
+  templateUrl: './who-we-are.html',
+  styleUrl: './who-we-are.scss'
+})
+export class WhoWeAre {
+
+}
