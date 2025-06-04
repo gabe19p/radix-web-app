2d374eeec12c452f4f0789108c46548859c66462
gabe19p <g19purselley@gmail.com>
2025-06-04 12:13:27 -0600

    careers page and nav link added

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { WhoWeAre } from './pages/who-we-are/who-we-are';
 import { WhatWeDo } from './pages/what-we-do/what-we-do';
+import { Careers } from './pages/careers/careers';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
   { path: 'who-we-are', component: WhoWeAre },
   { path: 'what-we-do', component: WhatWeDo },
+  { path: 'radix-careers', component: Careers },
 ];
