03ff01bda1309d572e217b523596648e0f298bbd
gabe19p <g19purselley@gmail.com>
2025-06-04 12:08:20 -0600

    what-we-do page added and linked to nav

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { WhoWeAre } from './pages/who-we-are/who-we-are';
+import { WhatWeDo } from './pages/what-we-do/what-we-do';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
   { path: 'who-we-are', component: WhoWeAre },
+  { path: 'what-we-do', component: WhatWeDo },
 ];
