30c452d4a8d5ca43a3cf5f5c68e708ed2439a3fd
gabe19p <g19purselley@gmail.com>
2025-06-08 20:24:10 -0600

    basic page for leadership information

---
+++ website/src/app/pages/leadership/leadership.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leadership',
+  imports: [],
+  templateUrl: './leadership.html',
+  styleUrl: './leadership.scss'
+})
+export class Leadership {
+
+}
