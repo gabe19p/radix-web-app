01011e387149e1ce1162404a8af8aecd360d356a
Gabe Purselley <g19purselley@gmail.com>
2025-07-24 08:56:58 -0600

    Finish footer

--- website/src/app/components/footer/footer.ts
+++ website/src/app/components/footer/footer.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
 export class Footer {
+  currentRoute = '';
 
+  constructor(private router: Router) {
+    // Update currentRoute whenever navigation changes
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
