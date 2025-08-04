d8dd1b5523bbd5032af9776a41e7d0a8d80652fc
Gabe Purselley <g19purselley@gmail.com>
2025-08-04 11:07:52 -0600

    removed dated leadership page

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Home } from './pages/home/home';
 import { WhoWeAre } from './pages/who-we-are/who-we-are';
 import { WhatWeDo } from './pages/what-we-do/what-we-do';
 import { Careers } from './pages/careers/careers';
-import { Leadership } from './pages/leadership/leadership';
 import { CapabilityPage } from './pages/capability-page/capability-page';
 
 export const routes: Routes = [
@@ -15,6 +14,5 @@ export const routes: Routes = [
   },
   { path: 'what-we-do', component: WhatWeDo },
   { path: 'careers', component: Careers },
-  { path: 'who-we-are/leadership', component: Leadership },
   { path: 'capabilities/:id', component: CapabilityPage },
 ];
