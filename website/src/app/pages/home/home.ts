fa8504ee9b540b0ddeeb120abd437226abe5f42e
Gabe Purselley <g19purselley@gmail.com>
2025-08-06 12:29:29 -0600

    fill homepage w/ actual content

--- website/src/app/pages/home/home.ts
+++ website/src/app/pages/home/home.ts
@@ -1,12 +1,30 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import { SvgList, svgList as rawSvgList } from '../../data/svg.data';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  svgList: SvgList;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.svgList = Object.fromEntries(
+      Object.entries(rawSvgList).map(([key, value]) => [
+        key,
+        {
+          ...value,
+          id: key,
+          icon: this.sanitizer.bypassSecurityTrustHtml(value.icon),
+        },
+      ])
+    );
+  }
+}
